2a70aa266e059cdfd0543046277c9359e63badc9
carluandraster <99754800+carluandraster@users.noreply.github.com>
2024-08-26 13:25:01 -0300

    Add files via upload

--- Práctica 1/Ejercicio 24/ejercicio24.c
+++ Práctica 1/Ejercicio 24/ejercicio24.c
@@ -4,12 +4,12 @@
 typedef unsigned short int byte;
 
 // Prototipos
-void leerMatriz(int[][], byte *, byte *);
-void buscar(int, int[][], byte, byte, byte *, byte *);
-void promedioColumnas(int[][], byte, byte, float[]);
-float promedioTotal(float[], byte);
-byte tieneCero(int[][], byte, byte);
-void superanPromedio(int[][], byte, byte);
+void leerMatriz(int[MAXELEM][MAXELEM], byte *, byte *);
+void buscar(int, int[MAXELEM][MAXELEM], byte, byte, byte *, byte *);
+void promedioColumnas(int[MAXELEM][MAXELEM], byte, byte, float[MAXELEM]);
+float promedioTotal(float[MAXELEM], byte);
+byte tieneCero(int[MAXELEM][MAXELEM], byte, byte);
+void superanPromedio(int[MAXELEM][MAXELEM], byte, byte, float);
 
 byte main()
 {
@@ -19,13 +19,13 @@ byte main()
     float PROMEDIOS[MAXELEM], promedio;
 
     // Entrada
-    leerMatriz(int MATRIZ, N *, M *);
+    leerMatriz(MATRIZ, &N, &M);
 
     // Inciso a
     printf("Busque un numero: ");
     scanf("%d", &x);
     buscar(x, MATRIZ, N, M, &i, &j);
-    i > N ? printf("No se encontro %d en la matriz\n", x) : printf("Fila: %d\nColumna: %d\n", i, j);
+    i > N ? printf("No se encontro %d en la matriz\n", x) : printf("Fila: %d\nColumna: %d\n", i + 1, j + 1);
 
     // Inciso b
     printf("Promedios de cada columna:\t");
@@ -33,60 +33,61 @@ byte main()
 
     // Inciso c
     promedio = promedioTotal(PROMEDIOS, M);
-    superanPromedio(MATRIZ, N, M);
+    superanPromedio(MATRIZ, N, M, promedio);
 
     return 0;
 }
 
-void leerMatriz(int MATRIZ[][], byte *N, byte *M)
+void leerMatriz(int MATRIZ[MAXELEM][MAXELEM], byte *N, byte *M)
 {
     byte i, j;
 
     do
     {
-        printf('Ingrese dimensiones de la matriz: ');
+        printf("Ingrese dimensiones de la matriz: ");
         scanf("%hu %hu", N, M);
     } while (*N > MAXELEM || *M > MAXELEM || *N == 0 || *M == 0);
 
     for (i = 0; i < *N; i++)
         for (j = 0; j < *M; j++)
         {
-            printf("Ingrese fila %hu columna %hu", i, j);
-            scanf("%d", MATRIZ[i][j]);
+            printf("Ingrese fila %hu columna %hu: ", i + 1, j + 1);
+            scanf("%d", &MATRIZ[i][j]);
         }
 }
 
-void buscar(int x, int MATRIZ[][], byte N, byte M, byte *i, byte *j)
+void buscar(int x, int MATRIZ[MAXELEM][MAXELEM], byte N, byte M, byte *i, byte *j)
 {
-    *i = 1;
-    *j = 1;
-    while (*i <= N && MATRIZ[*i][*j] != x)
+    *i = 0;
+    *j = 0;
+    while (*i < N && MATRIZ[*i][*j] != x)
     {
-        while (*j <= M && MATRIZ[*i][*j] != x)
+        while (*j < M && MATRIZ[*i][*j] != x)
             ++*j;
-        if (*j > M)
+        if (*j == M)
         {
-            *j = 1;
+            *j = 0;
             ++*i;
         }
     }
 }
 
-void promedioColumnas(int MATRIZ[][], byte N, byte M, float PROMEDIOS[])
+void promedioColumnas(int MATRIZ[MAXELEM][MAXELEM], byte N, byte M, float PROMEDIOS[])
 {
     byte i, j;
     int acumulador;
-    for (j = 1; j <= M; j++)
+    for (j = 0; j < M; j++)
     {
         acumulador = 0;
-        for (i = 1; i <= N; i++)
+        for (i = 0; i < N; i++)
             acumulador += MATRIZ[i][j];
-        PROMEDIOS[j] : = (float)acumulador / N;
+        PROMEDIOS[j] = (float)acumulador / N;
         printf("%f\t", PROMEDIOS[j]);
     }
+    printf("\n");
 }
 
-float promedioTotal(float PROMEDIOS[], byte M)
+float promedioTotal(float PROMEDIOS[MAXELEM], byte M)
 {
     byte i;
     float acumulador = 0;
@@ -95,4 +96,24 @@ float promedioTotal(float PROMEDIOS[], byte M)
     return acumulador / M;
 }
 
-byte tieneCero(int[][], byte, byte) {}
+byte tieneCero(int MATRIZ[MAXELEM][MAXELEM], byte FILA, byte M)
+{
+    byte i = 0;
+    while (i < M && MATRIZ[FILA][i] != 0)
+        i++;
+    return i < M;
+}
+
+void superanPromedio(int MATRIZ[MAXELEM][MAXELEM], byte N, byte M, float promedio)
+{
+    byte i, j, contador;
+    for (i = 0; i < N; i++)
+        if (tieneCero(MATRIZ, i, M))
+        {
+            contador = 0;
+            for (j = 0; j < M; j++)
+                if (MATRIZ[i][j] > promedio)
+                    contador++;
+            printf("La fila %hu tiene %hu numeros que superan el promedio de la matriz.\n", i + 1, contador);
+        }
+}
