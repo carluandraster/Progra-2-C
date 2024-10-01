4542dcf6ecdf81965febb6d1a0bd81bf4a0b4240
carluandraster <99754800+carluandraster@users.noreply.github.com>
2024-10-01 19:05:28 -0300

    Add files via upload

--- Práctica 6/Ejercicio 01/ejercicio1.c
+++ Práctica 6/Ejercicio 01/ejercicio1.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
+
 #define MAXELEM 100
 
 typedef struct nodo
@@ -10,51 +11,104 @@ typedef struct nodo
 typedef nodo *TLista;
 
 void cargarLista(TLista *L);
-void mostrarContenido(TLista L);
+void imprimirLista(TLista L);
 unsigned cantVocales(TLista L);
 short listaOrdenada(TLista L);
 void eliminar(TLista *L, unsigned P);
 
 int main()
 {
-    TLista L;
+    TLista lista = NULL;
     unsigned P;
-    cargarLista(&L);
-    mostrarContenido(L);
-    /*printf("Cantidad de vocales: %u\n", cantVocales(L));
-    printf(listaOrdenada(L) ? "La lista esta ordenada.\n" : "La lista no esta ordenada.\n");
+    cargarLista(&lista);
+    imprimirLista(lista);
+    printf("Cantidad de vocales: %u\n", cantVocales(lista));
+    printf(listaOrdenada(lista) ? "La lista esta ordenada.\n" : "La lista no esta ordenada.\n");
     printf("Ingrese una posicion: ");
     scanf("%u", &P);
-    eliminar(&L, P);
-    mostrarContenido(L);*/
+    eliminar(&lista, P);
+    imprimirLista(lista);
     return 0;
 }
 
 void cargarLista(TLista *L)
 {
     char cadena[MAXELEM];
-    nodo *act;
-    unsigned i = 1;
+    nodo *act, *prev = NULL;
+    unsigned i = 0;
+
     printf("Ingrese una cadena de texto.\n");
-    scanf("%s", cadena);
-    act = (nodo *)malloc(sizeof(nodo));
-    act->dato = cadena[0];
-    act->sig = NULL;
-    *L = act;
-    while (cadena[i] != '\0')
+    fgets(cadena, MAXELEM, stdin); // Leer una cadena completa con espacios
+
+    while (cadena[i] != '\0' && cadena[i] != '\n') // Evitar el salto de línea al final
     {
-        act = (nodo *)malloc(sizeof(nodo));
+        act = (nodo *)malloc(sizeof(nodo)); // Asignar memoria para nuevo nodo
         act->dato = cadena[i];
-        act = act->sig;
+        act->sig = NULL; // El siguiente nodo será NULL inicialmente
+
+        if (*L == NULL) // Si es el primer nodo, asignarlo como cabeza de la lista
+            *L = act;
+        else
+            prev->sig = act; // Enlazar el nodo anterior con el actual
+
+        prev = act; // Mover `prev` al nodo actual
         i++;
     }
 }
 
-void mostrarContenido(TLista L)
+// Función para imprimir la lista
+void imprimirLista(TLista L)
 {
-    while (L != NULL)
+    TLista aux = L;
+    while (aux != NULL)
     {
-        printf("%c", L->dato);
-        L = L->sig;
+        printf("%c", aux->dato);
+        aux = aux->sig;
     }
+    printf("\n");
+}
+
+unsigned cantVocales(TLista L)
+{
+    unsigned aux = 0;
+    TLista auxiliar;
+    auxiliar = L;
+    while (auxiliar != NULL)
+    {
+        aux += auxiliar->dato == 'A' || auxiliar->dato == 'a' || auxiliar->dato == 'E' || auxiliar->dato == 'e' || auxiliar->dato == 'I' || auxiliar->dato == 'i' || auxiliar->dato == 'O' || auxiliar->dato == 'o' || auxiliar->dato == 'U' || auxiliar->dato == 'u';
+        auxiliar = auxiliar->sig;
+    }
+    return aux;
+}
+
+short listaOrdenada(TLista L)
+{
+    TLista act, ant;
+    ant = L;
+    act = ant->sig;
+    while (act != NULL && ant->dato <= act->dato)
+    {
+        ant = act;
+        act = act->sig;
+    }
+    return act == NULL;
+}
+
+void eliminar(TLista *L, unsigned P)
+{
+    TLista ant, elim = *L, act = elim->sig;
+    unsigned i;
+    if (P == 1)
+        *L = act;
+    else
+    {
+        for (i = 1; i < P; i++)
+        {
+            ant = elim;
+            elim = elim->sig;
+            act = act->sig;
+        }
+        ant->sig = act;
+    }
+    free(elim);
 }
