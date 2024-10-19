e292c3baa113c225b70c10e1a10e90e522a74cbb
carluandraster <99754800+carluandraster@users.noreply.github.com>
2024-10-18 23:11:07 -0300

    Add files via upload

--- Práctica 7/Ejercicio 28/Uso/ejercicio28.c
+++ Práctica 7/Ejercicio 28/Uso/ejercicio28.c
@@ -1,46 +1,50 @@
 #include <stdio.h>
 #include "../Implementacion/N-ario.h"
 
-Nodo* crearNodo(TElementoA valor);
+Nodo *crearNodo(TElementoA valor);
 void cargarArbol(Arbol *a, Posicion *p);
-unsigned int cantidadNodos(Arbol a,Posicion p);
-unsigned int cantidadClavesPares(Arbol a,Posicion p);
+unsigned int cantidadNodos(Arbol a, Posicion p);
+unsigned int cantidadClavesPares(Arbol a, Posicion p);
 float porcentajeClavesPares(Arbol a, Posicion p);
-unsigned int gradoDeUnNodo(Arbol a,Posicion p);
-unsigned int grado(Arbol a);
-unsigned int nodosGradoImparEnNivelesImpares(Arbol a,unsigned int nivel);
-short int cumple(Arbol a);
-void CUMPLE(Arbol a,short int *Cumple);
-float promedioSi(Arbol a,unsigned int K,unsigned int nivel);
+unsigned int gradoDeUnNodo(Arbol a, Posicion p);
+void GRADO(Arbol a, Posicion p, unsigned int *grado);
+unsigned int nodosGradoImparEnNivelesImpares(Arbol a, Posicion p, unsigned int nivel);
+short int cumple(Arbol a, Posicion p);
+void CUMPLE(Arbol a, short int *Cumple);
+float promedioSi(Arbol a, unsigned int K, unsigned int nivel);
 
 int main()
 {
     Arbol a;
     short int Cumple;
-    unsigned int K;
+    unsigned int K, grado = 0;
     Posicion p;
 
-    cargarArbol(&a,&p);
-    printf("Cantidad de nodos: %u\n",cantidadNodos(a,p)); // Inciso a
-    printf("Porcentaje de claves pares: %f %%\n",porcentajeClavesPares(a,p)); // Inciso b
-    printf("Grado del arbol: %u\n",grado(a)); // Inciso c
+    cargarArbol(&a, &p);
+    printf("Cantidad de nodos: %u\n", cantidadNodos(a, p));                     // Inciso a
+    printf("Porcentaje de claves pares: %f %%\n", porcentajeClavesPares(a, p)); // Inciso b
+    // Inciso c
+    GRADO(a, p, &grado);
+    printf("Grado del arbol: %u\n", grado);
     // Inciso d
-    printf("Cantidad de nodos de grado impar en niveles impares: %u\n",nodosGradoImparEnNivelesImpares(a,1));
+    printf("Cantidad de nodos de grado impar en niveles impares: %u\n", nodosGradoImparEnNivelesImpares(a, p, 1));
     // Inciso e
-    printf(cumple(a) ? "(Funcion int) Para todas las claves salvo las de las hojas, su valor numerico es igual a la cantidad de hijos.\n" : "(Funcion int) Existen claves cuyos valores numericos no son iguales a sus cantidades de hijos.\n");
-    CUMPLE(a,&Cumple);
+    printf(cumple(a, p) ? "(Funcion int) Para todas las claves salvo las de las hojas, su valor numerico es igual a la cantidad de hijos.\n" : "(Funcion int) Existen claves cuyos valores numericos no son iguales a sus cantidades de hijos.\n");
+    CUMPLE(a, &Cumple);
     printf(Cumple ? "(Funcion void) Para todas las claves salvo las de las hojas, su valor numerico es igual a la cantidad de hijos.\n" : "(Funcion void) Existen claves cuyos valores numericos no son iguales a sus cantidades de hijos.\n");
     // Inciso f
     printf("Ingrese un nivel K: ");
-    scanf("%u",&K);
-    printf("Promedio de las claves del nivel K: %5.2f",promedioSi(a,K,1));
+    scanf("%u", &K);
+    printf("Promedio de las claves del nivel K: %5.2f", promedioSi(a, K, 1));
 
     return 0;
 }
 
-Nodo* crearNodo(TElementoA valor) {
-    Nodo* nuevo = (Nodo*)malloc(sizeof(Nodo));
-    if (nuevo != NULL) {
+Nodo *crearNodo(TElementoA valor)
+{
+    Nodo *nuevo = (Nodo *)malloc(sizeof(Nodo));
+    if (nuevo != NULL)
+    {
         nuevo->dato = valor;
         nuevo->hijoIzq = NULL;
         nuevo->hermanoDer = NULL;
@@ -48,15 +52,16 @@ Nodo* crearNodo(TElementoA valor) {
     return nuevo;
 }
 
-void cargarArbol(Arbol *a,Posicion *p) {
-    *a = crearNodo(1);  // Raíz del árbol
+void cargarArbol(Arbol *a, Posicion *p)
+{
+    *a = crearNodo(1); // Raíz del árbol
 
-    (*a)->hijoIzq = crearNodo(2);               // Hijo más izquierdo de la raíz
-    (*a)->hijoIzq->hermanoDer = crearNodo(3);   // Hermano derecho del primer hijo
+    (*a)->hijoIzq = crearNodo(2);                         // Hijo más izquierdo de la raíz
+    (*a)->hijoIzq->hermanoDer = crearNodo(3);             // Hermano derecho del primer hijo
     (*a)->hijoIzq->hermanoDer->hermanoDer = crearNodo(4); // Otro hermano
 
     // Agregamos hijos al nodo 2
-    (*a)->hijoIzq->hijoIzq = crearNodo(5);  
+    (*a)->hijoIzq->hijoIzq = crearNodo(5);
     (*a)->hijoIzq->hijoIzq->hermanoDer = crearNodo(6);
 
     // Agregamos un hijo al nodo 3
@@ -69,58 +74,121 @@ void cargarArbol(Arbol *a,Posicion *p) {
     *p = *a;
 }
 
-unsigned int cantidadNodos(Arbol a,Posicion p){
-    unsigned int contador = 0;
-    Posicion c;
-
-    if(!Nulo(p))
-    {
-        contador = 1;
-        c = HijoMasIzq(p,a);
-        while (c != NULL)
-        {
-            contador += 1+cantidadNodos(a,HijoMasIzq(c,a));
-            c = HermanoDer(c,a);
-        }
-    }
-
-    return contador;
-}
-
-unsigned int cantidadClavesPares(Arbol a,Posicion p){
-    unsigned int contador = 0;
-    Posicion c;
-
-    if(!Nulo(p))
-    {
-        contador = Info(p,a)%2 == 0;
-        c = HijoMasIzq(p,a);
-        while (c != NULL)
-        {
-            contador += (Info(c,a)%2 == 0)+cantidadClavesPares(a,HijoMasIzq(c,a));
-            c = HermanoDer(c,a);
-        }
-    }
-
-    return contador;
-}
-
-float porcentajeClavesPares(Arbol a, Posicion p){
-    return (float) cantidadClavesPares(a,p)/cantidadNodos(a,p)*100;
-}
-
-unsigned int gradoDeUnNodo(Arbol a,Posicion p){
+unsigned int cantidadNodos(Arbol a, Posicion p)
+{
     unsigned int contador = 0;
     Posicion c;
 
     if (!Nulo(p))
     {
-        c = HijoMasIzq(p,a);
+        contador = 1;
+        c = HijoMasIzq(p, a);
+        while (c != NULL)
+        {
+            contador += 1 + cantidadNodos(a, HijoMasIzq(c, a));
+            c = HermanoDer(c, a);
+        }
+    }
+
+    return contador;
+}
+
+unsigned int cantidadClavesPares(Arbol a, Posicion p)
+{
+    unsigned int contador = 0;
+    Posicion c;
+
+    if (!Nulo(p))
+    {
+        contador = Info(p, a) % 2 == 0;
+        c = HijoMasIzq(p, a);
+        while (c != NULL)
+        {
+            contador += (Info(c, a) % 2 == 0) + cantidadClavesPares(a, HijoMasIzq(c, a));
+            c = HermanoDer(c, a);
+        }
+    }
+
+    return contador;
+}
+
+float porcentajeClavesPares(Arbol a, Posicion p)
+{
+    return (float)cantidadClavesPares(a, p) / cantidadNodos(a, p) * 100;
+}
+
+unsigned int gradoDeUnNodo(Arbol a, Posicion p)
+{
+    unsigned int contador = 0;
+    Posicion c;
+
+    if (!Nulo(p))
+    {
+        c = HijoMasIzq(p, a);
         while (!Nulo(c))
         {
             contador++;
-            c = HermanoDer(c,a);
+            c = HermanoDer(c, a);
         }
     }
     return contador;
+}
+
+void GRADO(Arbol a, Posicion p, unsigned int *grado)
+{
+    Posicion c;
+    unsigned int contador = 0;
+
+    if (!Nulo(p))
+    {
+        c = HijoMasIzq(p, a);
+        while (c != NULL)
+        {
+            contador++;
+            GRADO(a, c, grado);
+            c = HermanoDer(c, a);
+        }
+        if (contador > *grado)
+            *grado = contador;
+    }
+}
+
+unsigned int nodosGradoImparEnNivelesImpares(Arbol a, Posicion p, unsigned int nivel)
+{
+    Posicion c;
+    unsigned int contador = 0;
+
+    if (!Nulo(p))
+    {
+        contador = gradoDeUnNodo(a, p) % 2 == 1 && nivel % 2 == 1;
+        c = HijoMasIzq(p, a);
+        while (!Nulo(c))
+        {
+            contador += nodosGradoImparEnNivelesImpares(a, c, nivel + 1);
+            c = HermanoDer(c, a);
+        }
+    }
+
+    return contador;
+}
+
+short int cumple(Arbol a, Posicion p)
+{
+    Posicion c;
+    short int aux = 1;
+
+    if (!Nulo(p))
+        if (Info(p, a) == gradoDeUnNodo(a, p))
+        {
+            c = HijoMasIzq(p, a);
+            while (!Nulo(c) && aux)
+            {
+                aux = aux && cumple(a, c);
+                c = HermanoDer(c, a);
+            }
+        }
+        else
+            return 0;
+
+    return aux;
 }
