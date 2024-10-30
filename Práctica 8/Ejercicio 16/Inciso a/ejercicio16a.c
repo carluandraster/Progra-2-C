7b52fddd48f1eabdb5f4de883f2d78a8032e913b
carluandraster <99754800+carluandraster@users.noreply.github.com>
2024-10-30 16:58:29 -0300

    Add files via upload

--- Práctica 8/Ejercicio 16/Inciso a/ejercicio16a.c
+++ Práctica 8/Ejercicio 16/Inciso a/ejercicio16a.c
@@ -2,8 +2,11 @@
 #define MAX_VERTICES 10
 #define INFINITO 64999
 
-void cargarDigrafo(unsigned int[][], unsigned int *);
-void Dijkstra(unsigned int[][], unsigned int, unsigned int, unsigned int D[], unsigned int P[]);
+void cargarDigrafo(unsigned int[][MAX_VERTICES], unsigned int *);
+unsigned int min(unsigned int, unsigned int);
+unsigned int posMinVal(unsigned int[], unsigned int, unsigned short int[]);
+short int todosVisitados(unsigned short int[], unsigned int);
+void Dijkstra(unsigned int[][MAX_VERTICES], unsigned int, unsigned int, unsigned int D[], unsigned int P[]);
 
 int main()
 {
@@ -29,4 +32,65 @@ void cargarDigrafo(unsigned int DG[MAX_VERTICES][MAX_VERTICES], unsigned int *N)
         for (j = 0; j < *N; j++)
             fscanf(archivo, "%u", &DG[i][j]);
     fclose(archivo);
+}
+
+unsigned int min(unsigned int a, unsigned int b)
+{
+    return a < b ? a : b;
+}
+
+unsigned int posMinVal(unsigned int D[], unsigned int N, unsigned short int VV[])
+{
+    unsigned int minAux = INFINITO, i, iMin;
+    for (i = 0; i < N; i++)
+        if (D[i] < minAux && !VV[i])
+        {
+            minAux = D[i];
+            iMin = i;
+        }
+
+    return iMin;
+}
+
+short int todosVisitados(unsigned short int VV[], unsigned int N)
+{
+    unsigned int i = 0;
+    while (i < N && VV[i])
+        i++;
+    return i == N;
+}
+
+void Dijkstra(unsigned int DG[][MAX_VERTICES], unsigned int N, unsigned int origen, unsigned int D[], unsigned int P[])
+{
+    unsigned short int VV[N];
+    unsigned int i, k, d;
+
+    // Inicialización
+    for (i = 0; i < N; i++)
+    {
+        VV[i] = 0;
+        if (DG[origen][i] > 0)
+        {
+            D[i] = DG[origen][i];
+            P[i] = 0;
+        }
+        else
+            P[i] = D[i] = INFINITO;
+    }
+    VV[origen] = 1;
+    P[origen] = D[origen] = 0;
+
+    while (!todosVisitados(VV, N))
+    {
+        k = posMinVal(D, N, VV);
+        VV[k] = 1;
+        for (i = 0; i < N; i++)
+            if (!VV[i] && DG[k][i] != 0)
+            {
+                d = D[i];
+                D[i] = min(D[i], D[k] + DG[k][i]);
+                if (d != D[i])
+                    P[i] = k + 1;
+            }
+    }
 }
---
+++ Práctica 8/Ejercicio 16/Inciso b/ejercicio16b.c
@@ -0,0 +1,61 @@
+#include <stdio.h>
+#define MAX_VERTICES 10
+#define INFINITO 64999
+
+void cargarDigrafo(unsigned int DG[][MAX_VERTICES], unsigned int *N);
+void Floyd(unsigned int DG[][MAX_VERTICES], unsigned int N, unsigned int P[][MAX_VERTICES]);
+void imprimirMatriz(unsigned int P[][MAX_VERTICES], unsigned int N);
+
+int main()
+{
+    unsigned int DG[MAX_VERTICES][MAX_VERTICES], N, P[MAX_VERTICES][MAX_VERTICES];
+    cargarDigrafo(DG, &N);
+    Floyd(DG, N, P);
+    imprimirMatriz(P, N);
+    return 0;
+}
+
+void cargarDigrafo(unsigned int DG[MAX_VERTICES][MAX_VERTICES], unsigned int *N)
+{
+    unsigned int i, j;
+    FILE *archivo;
+    archivo = fopen("../matriz.txt", "rt");
+    fscanf(archivo, "%u", N);
+    for (i = 0; i < *N; i++)
+        for (j = 0; j < *N; j++)
+            fscanf(archivo, "%u", &DG[i][j]);
+    fclose(archivo);
+}
+
+void Floyd(unsigned int DG[][MAX_VERTICES], unsigned int N, unsigned int P[][MAX_VERTICES])
+{
+    unsigned int i, j, k, A[MAX_VERTICES][MAX_VERTICES];
+
+    // Inicializar matriz A
+    for (i = 0; i < N; i++)
+        for (j = 0; j < N; j++)
+            if (i == j)
+                A[i][j] = 0;
+            else if (DG[i][j] != 0)
+                A[i][j] = DG[i][j];
+            else
+                A[i][j] = INFINITO;
+
+    // Algoritmo de Floyd
+    for (k = 0; k < N; k++)
+        for (i = 0; i < N; i++)
+            for (j = 0; j < N; j++)
+                if (A[i][k] + A[k][j] < A[i][j])
+                    A[i][j] = A[i][k] + A[k][j];
+}
+
+void imprimirMatriz(unsigned int P[][MAX_VERTICES], unsigned int N)
+{
+    unsigned int i, j;
+    for (i = 0; i < N; i++)
+    {
+        for (j = 0; j < N; j++)
+            printf("%u ", P[i][j]);
+        printf("\n");
+    }
+}
