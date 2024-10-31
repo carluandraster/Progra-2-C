2dcfe828e8f52d9f412128f9c1b4121fd43187bd
carluandraster <99754800+carluandraster@users.noreply.github.com>
2024-10-31 19:25:50 -0300

    Add files via upload

--- Práctica 8/Ejercicio 23/ejercicio23.c
+++ Práctica 8/Ejercicio 23/ejercicio23.c
@@ -2,26 +2,27 @@
 #define MAX_VERTICES 10
 #define INFINITO 64999
 
-void cargarDigrafo(unsigned int DG[][MAX_VERTICES],unsigned int *N);
-void Floyd(unsigned int DG[][MAX_VERTICES],unsigned int N,unsigned int P[][MAX_VERTICES]);
-void camino(unsigned int P[][MAX_VERTICES],unsigned int N,unsigned int v, unsigned int w);
+void cargarDigrafo(unsigned int DG[][MAX_VERTICES], unsigned int *N);
+void Floyd(unsigned int DG[][MAX_VERTICES], unsigned int N, unsigned int P[][MAX_VERTICES]);
+void camino(unsigned int P[][MAX_VERTICES], unsigned int N, unsigned int v, unsigned int w);
 
 int main()
 {
-    unsigned int DG[MAX_VERTICES][MAX_VERTICES],P[MAX_VERTICES][MAX_VERTICES],N,v,w;
-    cargarDigrafo(DG,&N);
-    Floyd(DG,N,P);
+    unsigned int DG[MAX_VERTICES][MAX_VERTICES], P[MAX_VERTICES][MAX_VERTICES], N, v, w;
+    cargarDigrafo(DG, &N);
+    Floyd(DG, N, P);
     do
     {
         printf("Ingrese origen: ");
-        scanf("%u",&v);
+        scanf("%u", &v);
     } while (v > N || v == 0);
     do
     {
         printf("Ingrese destino: ");
-        scanf("%u",&w);
+        scanf("%u", &w);
     } while (w > N || w == 0);
-    camino(P,N,v,w);
+    camino(P, N, v, w);
+    printf("%u\n", w);
 
     return 0;
 }
@@ -38,8 +39,9 @@ void cargarDigrafo(unsigned int DG[MAX_VERTICES][MAX_VERTICES], unsigned int *N)
     fclose(archivo);
 }
 
-void Floyd(unsigned int DG[][MAX_VERTICES],unsigned int N,unsigned int P[][MAX_VERTICES]){
-    unsigned int A[N][N],i,j,k;
+void Floyd(unsigned int DG[][MAX_VERTICES], unsigned int N, unsigned int P[][MAX_VERTICES])
+{
+    unsigned int A[N][N], i, j, k;
 
     // Inicializar A y P
     for (i = 0; i < N; i++)
@@ -53,7 +55,7 @@ void Floyd(unsigned int DG[][MAX_VERTICES],unsigned int N,unsigned int P[][MAX_V
             }
             else
                 A[i][j] = P[i][j] = INFINITO;
-            
+
     // Algoritmo de Floyd
     for (k = 0; k < N; k++)
         for (i = 0; i < N; i++)
@@ -61,17 +63,20 @@ void Floyd(unsigned int DG[][MAX_VERTICES],unsigned int N,unsigned int P[][MAX_V
                 if (A[i][k] + A[k][j] < A[i][j])
                 {
                     A[i][j] = A[i][k] + A[k][j];
-                    P[i][j] = k+1;
-                }         
+                    P[i][j] = k + 1;
+                }
 }
 
-void camino(unsigned int P[][MAX_VERTICES],unsigned int N,unsigned int v, unsigned int w){
-    if (P[v-1][w-1] == 0 && v == w)
-        printf("%u ",v);
-    else
+void camino(unsigned int P[][MAX_VERTICES], unsigned int N, unsigned int v, unsigned int w)
+{
+    if (v != 0 && w != 0)
     {
-        camino(P,N,v,P[v-1][w-1]);
-        printf("%u ",P[v-1][w-1]);
-        camino(P,N,P[v-1][w-1],w);
+        if (P[v - 1][w - 1] == 0)
+            printf("%u ", v);
+        else
+        {
+            camino(P, N, v, P[v - 1][w - 1]);
+            camino(P, N, P[v - 1][w - 1], w);
+        }
     }
 }
