dfb5a43060ceb35a54841541f5a6aee29833cfa6
carluandraster <99754800+carluandraster@users.noreply.github.com>
2024-10-30 22:08:08 -0300

    Add files via upload

--- Práctica 8/Ejercicio 16/Inciso b/ejercicio16b.c
+++ Práctica 8/Ejercicio 16/Inciso b/ejercicio16b.c
@@ -35,18 +35,24 @@ void Floyd(unsigned int DG[][MAX_VERTICES], unsigned int N, unsigned int P[][MAX
     for (i = 0; i < N; i++)
         for (j = 0; j < N; j++)
             if (i == j)
-                A[i][j] = 0;
-            else if (DG[i][j] != 0)
+                A[i][j] = P[i][j] = 0;
+            else if (DG[i][j] != 0) // (i,j) es una arista
+            {
                 A[i][j] = DG[i][j];
+                P[i][j] = 0;
+            }
             else
-                A[i][j] = INFINITO;
+                A[i][j] = P[i][j] = INFINITO;
 
     // Algoritmo de Floyd
     for (k = 0; k < N; k++)
         for (i = 0; i < N; i++)
             for (j = 0; j < N; j++)
                 if (A[i][k] + A[k][j] < A[i][j])
+                {
                     A[i][j] = A[i][k] + A[k][j];
+                    P[i][j] = k + 1;
+                }
 }
 
 void imprimirMatriz(unsigned int P[][MAX_VERTICES], unsigned int N)
