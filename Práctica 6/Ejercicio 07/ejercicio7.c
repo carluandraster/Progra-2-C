e1393ccb5e3e7431b8dbf8521bee6b0f40d17c69
carluandraster <99754800+carluandraster@users.noreply.github.com>
2024-11-09 19:06:13 -0300

    Add files via upload

--- Práctica 6/Ejercicio 07/ejercicio7.c
+++ Práctica 6/Ejercicio 07/ejercicio7.c
@@ -6,7 +6,7 @@
 
 typedef struct TLibro
 {
-    char titulo[MAXCAR],autor[MAXCAR];
+    char titulo[MAXCAR], autor[MAXCAR];
     int anio;
     struct TLibro *sig;
 } TLibro;
@@ -25,13 +25,15 @@ typedef struct TSocio
 } TSocio;
 typedef TSocio *TListaS;
 
-void agregarAutor(TListaA *A,char e[MAXCAR], TListaA *ult);
-void agregarSocio(TListaS *S,TSocio e, TListaS *ult);
-void agregarLibro(TLibro **L,char e[MAXCAR],char autor[MAXCAR],int anio, TLibro **ult);
+void agregarAutor(TListaA *A, char e[MAXCAR], TListaA *ult);
+void agregarSocio(TListaS *S, char e[MAXCAR], TListaS *ult);
+void agregarLibro(TLibro **L, char e[MAXCAR], char autor[MAXCAR], int anio, TLibro **ult);
 void cargarBiblioteca(TListaA AUTORES[MAXELEM]);
 void cargarSocios(TListaS *S);
-void registrarPrestamo(TListaA AUTORES[MAXELEM], TListaS S, char socio[MAXCAR],char autor[MAXCAR],char libro[MAXCAR]);
-void registrarDevoluciones(TListaA AUTORES[MAXELEM], TListaS S, char socio[MAXCAR],char autor[MAXCAR],char libro[MAXCAR]);
+void buscarAutor(TListaA AUTORES[], char nombre[], TListaA *autor);
+void buscarSocio(TListaS Socios, char nombre[], TListaS *socio);
+void registrarPrestamo(TListaA AUTORES[MAXELEM], TListaS S, char socio[], char autor[], char libro[]);
+void registrarDevoluciones(TListaA AUTORES[MAXELEM], TListaS S, char socio[], char autor[], char libro[]);
 void mostrarAutores(TListaA AUTORES[MAXELEM]);
 void mostrarSocios(TListaS S);
 
@@ -42,9 +44,9 @@ int main()
 
     cargarBiblioteca(AUTORES);
     cargarSocios(&Socios);
-    registrarPrestamo(AUTORES,Socios,"Andres_Efstratiadis","Brian_Kernighan","El_lenguaje_de_programacion_C");
-    registrarPrestamo(AUTORES,Socios,"Andres_Efstratiadis","Nikolai_Piskunov","Calculo_Diferencial_e_Integral");
-    registrarDevoluciones(AUTORES,Socios,"Laureano_Ahmad","Adam_Smith","La_riqueza_de_las_naciones");
+    registrarPrestamo(AUTORES, Socios, "Andres_Efstratiadis", "Brian_Kernighan", "El_lenguaje_de_programacion_C");
+    registrarPrestamo(AUTORES, Socios, "Andres_Efstratiadis", "Nikolai_Piskunov", "Calculo_Diferencial_e_Integral");
+    registrarDevoluciones(AUTORES, Socios, "Laureano_Ahmad", "Adam_Smith", "La_riqueza_de_las_naciones");
     mostrarAutores(AUTORES);
     mostrarSocios(Socios);
 
@@ -52,11 +54,13 @@ int main()
 }
 
 // Agrega un autor a una lista de autores
-void agregarAutor(TListaA *A,char e[MAXCAR], TListaA *ult){
+void agregarAutor(TListaA *A, char e[MAXCAR], TListaA *ult)
+{
     TListaA aux = *ult, nuevo;
     nuevo = (TListaA)malloc(sizeof(TAutor));
-    strcpy(nuevo->nombre,e);
-    nuevo->sig = nuevo->libros = NULL;
+    strcpy(nuevo->nombre, e);
+    nuevo->sig = NULL;
+    nuevo->libros = NULL;
     if (*A == NULL)
         *A = *ult = nuevo;
     else
@@ -67,10 +71,12 @@ void agregarAutor(TListaA *A,char e[MAXCAR], TListaA *ult){
 }
 
 // Agrega un socio a la lista de socios
-void agregarSocio(TListaS *S,TSocio e, TListaS *ult){
+void agregarSocio(TListaS *S, char e[MAXCAR], TListaS *ult)
+{
     TListaS aux = *ult, ant, nuevo;
     nuevo = (TListaS)malloc(sizeof(TSocio));
-    *nuevo = e;
+    strcpy(nuevo->nombre, e);
+    nuevo->libros = NULL;
     nuevo->sig = NULL;
     if (*S == NULL)
         *S = *ult = nuevo;
@@ -82,12 +88,13 @@ void agregarSocio(TListaS *S,TSocio e, TListaS *ult){
 }
 
 // Agrega un libro a una sublista de libros
-void agregarLibro(TLibro **L,char e[MAXCAR],char autor[MAXCAR],int anio, TLibro **ult){
+void agregarLibro(TLibro **L, char e[MAXCAR], char autor[MAXCAR], int anio, TLibro **ult)
+{
     TLibro *aux, *nuevo;
     aux = *L;
     nuevo = (TLibro *)malloc(sizeof(TLibro));
     strcpy(nuevo->titulo, e);
-    strcpy(nuevo->autor,autor);
+    strcpy(nuevo->autor, autor);
     nuevo->anio = anio;
     nuevo->sig = NULL;
     if (aux == NULL)
@@ -100,25 +107,180 @@ void agregarLibro(TLibro **L,char e[MAXCAR],char autor[MAXCAR],int anio, TLibro 
 }
 
 // Carga la biblioteca
-void cargarBiblioteca(TListaA AUTORES[MAXELEM]){
-    int i,anio;
+void cargarBiblioteca(TListaA AUTORES[MAXELEM])
+{
+    int i, anio;
     FILE *archivo;
-    char nombreAutor[MAXCAR],nombre;
+    char nombreAutor[MAXCAR], nombre[MAXCAR];
     TListaA ult;
     TLibro *Ult;
 
     for (i = 0; i < MAXELEM; i++)
         AUTORES[i] = NULL;
-    archivo = fopen("autores.txt","rt");
-    while (fscanf(archivo,"%s",nombreAutor))
+    archivo = fopen("autores.txt", "rt");
+    while (fscanf(archivo, "%31s\n", nombreAutor))
     {
-        agregarAutor(&AUTORES[nombreAutor[0]-65],nombreAutor,&ult);
-        while (fscanf(archivo,"%s %d",nombre,&anio))
-            agregarLibro(ult->libros,nombre,nombreAutor,anio,Ult);
+        agregarAutor(&AUTORES[nombreAutor[0] - 65], nombreAutor, &ult);
+        while (fscanf(archivo, "%31s %d\n", nombre, &anio)) // Ciclo infinito
+            agregarLibro(&ult->libros, nombre, nombreAutor, anio, &Ult);
     }
     fclose(archivo);
 }
 
-void cargarSocios(TListaS *S){
+// Carga la lista de socios
+void cargarSocios(TListaS *S)
+{
+    int anio;
+    FILE *archivo;
+    char nombreAutor[MAXCAR], nombreSocio[MAXCAR], nombreLibro[MAXCAR];
+    TListaS ult;
+    TLibro *Ult;
 
+    archivo = fopen("socios.txt", "rt");
+    while (fscanf(archivo, "%s", nombreSocio))
+    {
+        agregarSocio(S, nombreSocio, &ult);
+        while (fscanf(archivo, "%s %s %d", nombreAutor, nombreLibro, &anio))
+            agregarLibro(&ult->libros, nombreLibro, nombreAutor, anio, &Ult);
+    }
+    fclose(archivo);
+}
+
+// Dado el nombre de un autor, devuelve un puntero a ese autor (NULL si el autor no existe)
+void buscarAutor(TListaA AUTORES[], char nombre[], TListaA *autor)
+{
+    *autor = AUTORES[nombre[0] - 65];
+    while (*autor != NULL && strcmp((*autor)->nombre, nombre) < 0)
+        *autor = (*autor)->sig;
+    if (strcmp((*autor)->nombre, nombre) > 0)
+        *autor = NULL;
+}
+
+// Dado el nombre de un socio, devuelve un puntero al socio si existe
+void buscarSocio(TListaS Socios, char nombre[], TListaS *socio)
+{
+    *socio = Socios;
+    while (*socio != NULL && strcmp(nombre, (*socio)->nombre))
+        *socio = (*socio)->sig;
+}
+
+void registrarPrestamo(TListaA AUTORES[MAXELEM], TListaS S, char socio[MAXCAR], char autor[MAXCAR], char libro[MAXCAR])
+{
+    TListaA act;
+    TLibro *Libro, *ant;
+    TListaS aux;
+
+    buscarAutor(AUTORES, autor, &act);
+    if (act != NULL) // Se encontro al autor
+    {
+        buscarSocio(S, socio, &aux);
+        if (aux != NULL) // Socio encontrado
+        {
+            // Buscar libro
+            Libro = act->libros;
+            if (Libro != NULL && !strcmp(Libro->titulo, libro)) // El libro es el primero de la lista
+            {
+                // Mover libro de biblioteca a socio
+                act->libros = Libro->sig;
+                Libro->sig = aux->libros;
+                aux->libros = Libro;
+            }
+            else
+            {
+                // Seguir buscando libro
+                while (Libro != NULL && strcmp(Libro->titulo, libro))
+                {
+                    ant = Libro;
+                    Libro = Libro->sig;
+                }
+                if (Libro != NULL) // Libro encontrado
+                {
+                    ant->sig = Libro->sig;
+                    Libro->sig = aux->libros;
+                    aux->libros = Libro;
+                }
+            }
+        }
+    }
+}
+
+void registrarDevoluciones(TListaA AUTORES[MAXELEM], TListaS S, char socio[MAXCAR], char autor[MAXCAR], char libro[MAXCAR])
+{
+    TListaA act;
+    TLibro *Libro, *ant;
+    TListaS aux;
+
+    buscarAutor(AUTORES, autor, &act);
+    if (act != NULL)
+    {
+        buscarSocio(S, socio, &aux);
+        if (aux != NULL)
+        {
+            Libro = aux->libros;
+            if (Libro != NULL && !strcmp(Libro->titulo, libro)) // El libro es el primero de la lista
+            {
+                aux->libros = Libro->sig;
+                Libro->sig = act->libros;
+                act->libros = Libro;
+            }
+            else
+            {
+                while (Libro != NULL && strcmp(Libro->titulo, libro))
+                {
+                    ant = Libro;
+                    Libro = Libro->sig;
+                }
+                if (Libro != NULL)
+                {
+                    ant->sig = Libro->sig;
+                    Libro->sig = act->libros;
+                    act->libros = Libro;
+                }
+            }
+        }
+    }
+}
+
+void mostrarAutores(TListaA AUTORES[MAXELEM])
+{
+    int i;
+    TListaA aux;
+    TLibro *Aux;
+
+    for (i = 0; i < MAXELEM; i++)
+        if (AUTORES[i] != NULL)
+        {
+            aux = AUTORES[i];
+            while (aux != NULL)
+            {
+                printf("%s\n", aux->nombre);
+                Aux = aux->libros;
+                while (Aux != NULL)
+                {
+                    printf("%s %d\n", Aux->titulo, Aux->anio);
+                    Aux = Aux->sig;
+                }
+                printf("\n");
+                aux = aux->sig;
+            }
+        }
+}
+
+void mostrarSocios(TListaS S)
+{
+    TListaS aux = S;
+    TLibro *Aux;
+
+    while (aux != NULL)
+    {
+        printf("%s\n", aux->nombre);
+        Aux = aux->libros;
+        while (Aux != NULL)
+        {
+            printf("%s %s %d\n", Aux->autor, Aux->titulo, Aux->anio);
+            Aux = Aux->sig;
+        }
+        printf("\n");
+        aux = aux->sig;
+    }
 }
